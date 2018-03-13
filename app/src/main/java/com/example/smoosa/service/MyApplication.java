823dc321829bbddabab97b53d952810d2c581060
ArchanaHemukumar <archanahemukumar@gmail.com>
2018-03-13 15:22:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/smoosa/service/MyApplication.java
@@ -0,0 +1,28 @@
+package com.example.smoosa.service;
+
+import android.app.Application;
+
+/**
+ * Created by smoosa on 3/12/18.
+ */
+
+public class MyApplication extends Application {
+
+    private static MyApplication mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mInstance = this;
+    }
+
+    public static synchronized MyApplication getInstance() {
+        return mInstance;
+    }
+
+    public void setConnectivityListener(ConnectivityReceiver.ConnectivityReceiverListener listener) {
+        ConnectivityReceiver.connectivityReceiverListener = listener;
+    }
+
+}
